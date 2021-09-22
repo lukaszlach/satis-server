9cad49a5dca86963bb8f1b5a14d39227b989c541
Bonno <GitHub@bonno.bo-ma.nl>
2021-09-22 18:29:10 +0200

    Fixed regular expression not accepting the given ip (cidr notation) (#12)
    * Fixed regular expression not accepting the given ip (cidr notation)
    
    * Add missing package tini
    
    * Add nginx config to allow for a proxy placed in front of satis container
    
    * Add nginx config to allow basic auth

--- bin/satis-server.sh
+++ bin/satis-server.sh
@@ -9,7 +9,13 @@ cp -fr /satis-server/nginx/* /etc/nginx/
 if [ -f /etc/satis-server/https/cert.pem ] && [ -f /etc/satis-server/https/key.pem ]; then
     sed 's/^#ssl//g; s/$SSL_PORT/'"$SSL_PORT"'/g' -i /etc/nginx/conf.d/*.conf
 fi
-if echo -n "$API_ALLOW" | grep -E "(\d\.)+\d/\d+" > /dev/null; then
+if [ -f /etc/satis-server/htpasswd ]; then
+    sed 's/^#auth//g' -i /etc/nginx/conf.d/*.conf
+fi
+if echo -n "$PROXY_IP" | grep -E "(\d{1,3}\.)+\d{1,3}/\d{1,2}" > /dev/null; then
+	sed 's/^#proxy //g; s#$PROXY_IP#'"$PROXY_IP"'#g' -i /etc/nginx/conf.d/*.conf
+fi
+if echo -n "$API_ALLOW" | grep -E "(\d{1,3}\.)+\d{1,3}/\d{1,2}" > /dev/null; then
     sed 's/^#allow//g; s#$API_ALLOW#'"$API_ALLOW"'#g' -i /etc/nginx/conf.d/*.conf
 fi
 if [ ! -z "$PUSH_SECRET" ]; then
